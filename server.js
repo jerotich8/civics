02057efe034ce943a95e9465c494bd76e4f41cfb
jerotich8 <ssumukwo621@gmail.com>
2024-11-29 16:08:37 +0300

    defined routes

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const dotenv = require ("dotenv")
+const app = express();
+
+
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+dotenv.config();
+
+
+app.use('/civics/api/users', require('./routes/auth'));
+app.use('/civics/api', require('./routes/civics'));
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`);
+});
